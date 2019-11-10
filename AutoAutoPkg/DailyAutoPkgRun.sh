4b13ce4b54c60659d0dae7609cf340e15cc4cd72
ScottGary <scott.gary@mfj.io>
2019-11-10 12:08:04 -0500

    Update DailyAutoPkgRun.sh

--- AutoAutoPkg/DailyAutoPkgRun.sh
+++ AutoAutoPkg/DailyAutoPkgRun.sh
@@ -28,6 +28,104 @@ JamfPolicyList="/Users/Shared/AutoAutoPkg/AutoAutoPkgPolicies.txt"
 ReportList="/Users/Shared/AutoAutoPkg/AutoPkgRun.plist"
 recipe_list="/Users/Shared/AutoAutoPkg/recipe_list.txt"  # see https://github.com/autopkg/autopkg/wiki/Running-Multiple-Recipes for setup of .txt file
 
+JamfCheck(){
+  if [ -z "$JamfServer" ]; then
+    echo "No Jamf server found; exiting"
+    exit 1
+  else
+    if [ -z "$JSSUser" ]; then
+      echo "No Jamf user account found; exiting"
+      exit 1
+    else
+      if [ -z "$JSSPass" ]; then
+        echo "No Jamf user password found; exiting"
+        exit 1
+      else
+        echo "Checking JSS Permissions"
+        SwTitlesCheck=$(curl -s -u "$JSSUser:$JSSPass" -X GET "$JamfServer/JSSResource/patchsoftwaretitles")
+        PatchCheck=$(curl -s -u "$JSSUser:$JSSPass" -X GET "$JamfServer/JSSResource/patchpolicies")
+        PolicyCheck=$(curl -s -u "$JSSUser:$JSSPass" -X GET "$JamfServer/JSSResource/policies")
+        if [ -z "$SwTitlesCheck" ]; then
+          echo "Permissions to Software Titles not set; exiting"
+          exit 1
+        else
+          if [ -z "$PatchCheck" ]; then
+            echo "Permissions to Patch Policies not set; exiting"
+            exit 1
+          else
+            if [ -z "$PolicyCheck" ]; then
+              echo "Permissions to Policies not set; exiting"
+              exit 1
+            else
+              echo "All Jamf read permissions set"
+            fi
+          fi
+        fi
+      fi
+    fi
+  fi
+}
+
+SlackNotify(){
+  AppAndVersion="$1"
+  if [ -z "$SlackHook" ]; then
+    echo "No webhook url found; skipping"
+  else
+    if [ -z "$Channel" ]; then
+      echo "No Slack desination found; skipping"
+    else
+      if [ -z "$Username" ]; then
+        echo "No Slack name specified; skipping"
+      else
+        if [ -z "$EMOJI" ]; then
+          echo "No user icon specified; skipping"
+        else
+          if [ -z "$AppAndVersion" ]; then
+            SlackMessage="Daily AutoPkg Run:\nNo new titles"
+            Payload="payload={\"channel\": \"$Channel\", \"username\": \"$Username\", \"text\": \"$SlackMessage\", \"icon_emoji\": \"$EMOJI\"}"
+            curl -s -X POST --data-urlencode "$Payload" "$SlackHook"
+          else
+            SlackMessage="New Software Available: \n$AppAndVersion"
+            Payload="payload={\"channel\": \"$Channel\", \"username\": \"$Username\", \"text\": \"$SlackMessage\", \"icon_emoji\": \"$EMOJI\"}"
+            curl -s -X POST --data-urlencode "$Payload" "$SlackHook"
+          fi
+        fi
+      fi
+    fi
+  fi
+}
+
+AutoPkgCheck(){
+  if [ -z "$SoftwareTitles" ]; then
+    echo "No software titles list found; exiting"
+    exit 1
+  else
+    if [ -z "$JamfPolicyList" ]; then
+      echo "No Jamf Policy list found; exiting"
+      exit 1
+    else
+      if [ -z "$ReportList" ]; then
+        echo "No outpput path found; exiting"
+        exit 1
+      else
+        if [ -z "$recipe_list" ]; then
+          echo "No recipe list found; exiting"
+          exit 1
+        else
+          AutoPkgRepo=$(defaults read com.github.autopkg CACHE_DIR)
+          if [ -z "$AutoPkgRepo" ]; then
+            AutoPkgRepo="$HOME/Library/AutoPkg/Cache"
+            export AutoPkgRepo="$AutoPkgRepo"
+          else
+            echo "All AutoPkg veriables verified"
+            export AutoPkgRepo="$AutoPkgRepo"
+          fi
+        fi
+      fi
+    fi
+  fi
+}
+
 PkgUpload(){
   PkgName="$1"
   PkgPath="$2"
@@ -52,20 +150,32 @@ PatchDef(){
   # Patch Server Update
   AppName="$1"
   AppPath="$2"
-  if [ -z "$AppPath" ]; then
-    echo "No .app found; skipping Path Def"
+  if [ -z "$PatchServer" ]; then
+    echo "No patch server defined; skipping"
   else
-    curl -s -X POST \
-      "$PatchServer/$AppName/version?=" \
-      -H 'Accept: */*' \
-      -H "Authorization: Bearer $PatchBearer" \
-      -H 'Content-Type: application/json' \
-      -H 'Host: jamf-patch.mfj.io' \
-      -d "$(python "$PatchScript" "$AppPath" --patch-only)"
+    if [ -z "$PatchBearer" ]; then
+      echo "No API token found; skippimg"
+    else
+      if [ -z "$PatchScript" ]; then
+        echo "No patching script found; skipping"
+      else
+        if [ -z "$AppPath" ]; then
+          echo "No .app found; skipping Path Def"
+        else
+          curl -s -X POST \
+            "$PatchServer/$AppName/version?=" \
+            -H 'Accept: */*' \
+            -H "Authorization: Bearer $PatchBearer" \
+            -H 'Content-Type: application/json' \
+            -H 'Host: jamf-patch.mfj.io' \
+            -d "$(python "$PatchScript" "$AppPath" --patch-only)"
+        fi
+      fi
+    fi
   fi
 }
 
-PatchUpadte(){
+PatchUpdate(){
   # Find ID's
   AppName="$1"
   ReleaseVersion="$2"
@@ -108,40 +218,6 @@ InstallUpdate(){
   fi
 }
 
-AutoPkgCheck(){
-  if [ -z "$SoftwareTitles" ]; then
-    echo "No software titles list found; exiting"
-    exit 1
-  else
-    SoftwareTitles="$SoftwareTitles"
-  fi
-  if [ -z "$JamfPolicyList" ]; then
-    echo "No Jamf Policy list found; exiting"
-    exit 1
-  else
-    JamfPolicyList="$JamfPolicyList"
-  fi
-  if [ -z "$ReportList" ]; then
-    echo "No outpput path found; exiting"
-    exit 1
-  else
-    ReportList="$ReportList"
-  fi
-  if [ -z "$recipe_list" ]; then
-    echo "No recipe list found; exiting"
-    exit 1
-  else
-    recipe_list="$recipe_list"
-  fi
-  AutoPkgRepo=$(defaults read com.github.autopkg CACHE_DIR)
-  if [ -z "$AutoPkgRepo" ]; then
-    AutoPkgRepo="$HOME/Library/AutoPkg/Cache"
-    export AutoPkgRepo="$AutoPkgRepo"
-  else
-    AutoPkgRepo="$AutoPkgRepo"
-  fi
-}
-
 AutoPkgRun(){
   # AutoPkg Run:
   /usr/local/bin/autopkg run --recipe-list="$recipe_list" --report-plist "$ReportList"
@@ -151,9 +227,7 @@ AutoPkgRun(){
   # If nothing we are sending a slack notification and exiting:
   if [ -z "$AutoPkgResults" ]; then
     # Slack Webhook
-    SlackMessage="Daily AutoPkg Run:\nNo new titles"
-    Payload="payload={\"channel\": \"$Channel\", \"username\": \"$Username\", \"text\": \"$SlackMessage\", \"icon_emoji\": \"$EMOJI\"}"
-    curl -s -X POST --data-urlencode "$Payload" "$SlackHook"
+    SlackNotify
     exit 0
   else
     # If we made it here there are new builds. Make array of new run:
@@ -174,12 +248,11 @@ AutoPkgRun(){
     done
     #Slack Notification
     AppAndVersion=$(/usr/libexec/PlistBuddy -c "Print :summary_results:pkg_creator_summary_result" "$ReportList" | grep "pkg_path" | awk '{print $3, $4}' | awk -F "/" '{print $6}' | awk -F "-" '{print $1,$2}' | sed 's/\.pkg//g')
-    SlackMessage="New Software Available: \n$AppAndVersion"
-    Payload="payload={\"channel\": \"$Channel\", \"username\": \"$Username\", \"text\": \"$SlackMessage\", \"icon_emoji\": \"$EMOJI\"}"
-    curl -s -X POST --data-urlencode "$Payload" "$SlackHook"
+    SlackNotify "$AppAndVersion"
   fi
 }
 
+JamfCheck
 AutoPkgCheck
 AutoPkgRun
 
