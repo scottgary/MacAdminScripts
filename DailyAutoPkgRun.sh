87a574fddd61171927e7e59abf7e6f75803bc24b
ScottGary <scott.gary@mfj.io>
2019-11-10 10:35:05 -0500

    automationscripts

---
+++ DailyAutoPkgRun.sh
@@ -0,0 +1,186 @@
+#!/bin/bash
+################################################################################
+#  AutoAutoPkg    Created by: Scott Gary                                       #
+#  Version History:                                                            #
+#                  11/7/19 inital structure and design                         #
+#                  11/10/19 Better error handling                              #
+################################################################################
+
+# Jamf Variables:
+JamfServer="" # full https:// address with :{port}
+JSSUser="" # Must have permissions for packages, polices, patch policies and Patch Management Software Titles c,r,u
+JSSPass=""
+
+# Patch Server Variables:
+PatchServer="" # "/$AppName/Version" will be called after
+PatchBearer="" # API Token
+PatchScript="/Users/Shared/patchstarter.py"
+
+# Slack Variables
+SlackHook="" #Webhook URL
+Channel=""
+Username=""
+EMOJI="" # Used as user icon
+
+# AutoPkg Variables:
+SoftwareTitles="/Users/Shared/AutoAutoPkg/AutoAutoPkgSoftwareTitles.txt"
+JamfPolicyList="/Users/Shared/AutoAutoPkg/AutoAutoPkgPolicies.txt"
+ReportList="/Users/Shared/AutoAutoPkg/AutoPkgRun.plist"
+recipe_list="/Users/Shared/AutoAutoPkg/recipe_list.txt"  # see https://github.com/autopkg/autopkg/wiki/Running-Multiple-Recipes for setup of .txt file
+
+PkgUpload(){
+  PkgName="$1"
+  PkgPath="$2"
+  if [ -z "$PkgPath" ]; then
+    echo "Only download policy found; skipping Jamf upload"
+  else
+    #Upload to Jamf
+    PkgUpload=$(curl -u $JSSUser:$JSSPass -X POST \
+      "$JamfServer"/dbfileupload \
+      -H 'DESTINATION: 0' \
+      -H 'OBJECT_ID: -1' \
+      -H 'FILE_TYPE: 0' \
+      -H 'FILE_NAME: '$PkgName \
+      -T $PkgPath)
+    # Get New Package ID
+    PkgId=$(echo "$PkgUpload" | awk -F "<id>" '{print $2}' | awk -F "</id" '{print $1}')
+    export PkgId="$PkgId"
+  fi
+}
+
+PatchDef(){
+  # Patch Server Update
+  AppName="$1"
+  AppPath="$2"
+  if [ -z "$AppPath" ]; then
+    echo "No .app found; skipping Path Def"
+  else
+    curl -s -X POST \
+      "$PatchServer/$AppName/version?=" \
+      -H 'Accept: */*' \
+      -H "Authorization: Bearer $PatchBearer" \
+      -H 'Content-Type: application/json' \
+      -H 'Host: jamf-patch.mfj.io' \
+      -d "$(python "$PatchScript" "$AppPath" --patch-only)"
+  fi
+}
+
+PatchUpadte(){
+  # Find ID's
+  AppName="$1"
+  ReleaseVersion="$2"
+  PatchVersionId=$(cat "$SoftwareTitles" | grep -i "$AppName" | awk -F " " '{print $2}')
+  if [ -z "$PatchVersionId" ]; then
+    echo "No software title id found; skipping patch definition update"
+  else
+    PatchId=$(cat "$JamfPolicyList" | grep -i "$AppName" | awk -F " " '{print $3}' )
+    # Get Jamf Version
+    JamfVersion=$(curl -sv -H "Accept: application/xml" -u "$JSSUser":"$JSSPass" "$JamfServer/JSSResource/patchsoftwaretitles/id/$PatchVersionId" -X GET)
+    until [ "$ReleaseVersion" == "$JamfVersion" ]
+    do
+      JamfVersion=$(curl -sv -H "Accept: application/xml" -u "$JSSUser":"$JSSPass" "$JamfServer/JSSResource/patchsoftwaretitles/id/$PatchVersionId" -X GET)
+    done
+    # Update Patch Def
+    DefPkgChange="<patch_software_title><versions><version><software_version>$JamfVersion</software_version><package><id>$PkgId</id></package></version></versions></patch_software_title>"​
+    curl -s -u $JSSUser:$JSSPass "$JamfServer/JSSResource/patchsoftwaretitles/id/$PatchVersionId" -X PUT \
+    -H "Content-type: application/xml" \
+    -d "<?xml version=\"1.0\" encoding=\"UTF-8\"?>$DefPkgChange"
+    # Update Patch Policy
+    PatchChange="<patch_policy><general><target_version>$JamfVersion</target_version></general></patch_policy>"​
+    curl -s -u $JSSUser:$JSSPass "$JamfServer/JSSResource/patchpolicies/id/$PatchId" -X PUT \
+    -H "Content-type: application/xml" \
+    -d "<?xml version=\"1.0\" encoding=\"UTF-8\"?>$PatchChange"
+  fi
+}
+
+InstallUpdate(){
+  # Update Install Policy
+  PkgId="$1"
+  AppName="$2"
+  InstallId=$(cat "$JamfPolicyList" | grep -i "$AppName" | awk -F " " '{print $2}' )
+  if [ -z "$InstallId" ]; then
+    echo "No Jamf install policy id found; skipping update"
+  else
+    InstallPkgChange="<policy><package><package_configuration><packages><package><id>$PkgId</id></package></packages></package_configuration></policy>"​
+    curl -s -u $JSSUser:$JSSPass "$JamfServer/JSSResource/policies/id/$InstallId" -X PUT \
+    -H "Content-type: application/xml" \
+    -d "<?xml version=\"1.0\" encoding=\"UTF-8\"?>$InstallPkgChange"
+  fi
+}
+
+AutoPkgCheck(){
+  if [ -z "$SoftwareTitles" ]; then
+    echo "No software titles list found; exiting"
+    exit 1
+  else
+    SoftwareTitles="$SoftwareTitles"
+  fi
+  if [ -z "$JamfPolicyList" ]; then
+    echo "No Jamf Policy list found; exiting"
+    exit 1
+  else
+    JamfPolicyList="$JamfPolicyList"
+  fi
+  if [ -z "$ReportList" ]; then
+    echo "No outpput path found; exiting"
+    exit 1
+  else
+    ReportList="$ReportList"
+  fi
+  if [ -z "$recipe_list" ]; then
+    echo "No recipe list found; exiting"
+    exit 1
+  else
+    recipe_list="$recipe_list"
+  fi
+  AutoPkgRepo=$(defaults read com.github.autopkg CACHE_DIR)
+  if [ -z "$AutoPkgRepo" ]; then
+    AutoPkgRepo="$HOME/Library/AutoPkg/Cache"
+    export AutoPkgRepo="$AutoPkgRepo"
+  else
+    AutoPkgRepo="$AutoPkgRepo"
+  fi
+}
+
+AutoPkgRun(){
+  # AutoPkg Run:
+  /usr/local/bin/autopkg run --recipe-list="$recipe_list" --report-plist "$ReportList"
+
+  # Check for new pkgs:
+  AutoPkgResults=$(/usr/libexec/PlistBuddy -c "Print :summary_results:pkg_creator_summary_result" "$ReportList" | grep "pkg_path" | awk '{print $3}')
+  # If nothing we are sending a slack notification and exiting:
+  if [ -z "$AutoPkgResults" ]; then
+    # Slack Webhook
+    SlackMessage="Daily AutoPkg Run:\nNo new titles"
+    Payload="payload={\"channel\": \"$Channel\", \"username\": \"$Username\", \"text\": \"$SlackMessage\", \"icon_emoji\": \"$EMOJI\"}"
+    curl -s -X POST --data-urlencode "$Payload" "$SlackHook"
+    exit 0
+  else
+    # If we made it here there are new builds. Make array of new run:
+    declare -a ResultArray
+    mapfile -t ResultArray < <(/usr/libexec/PlistBuddy -c "Print :summary_results:pkg_creator_summary_result" "$ReportList" | grep "pkg_path" | awk '{print $3, $4}' | awk -F "/" '{print $6}' | awk -F "-" '{print $1}')
+    for i in "${ResultArray[@]}"
+    do
+      AppName="$i"
+      AppPath=$(find "$AutoPkgRepo" -iname "$AppName.app")
+      PkgPath=$(/usr/libexec/PlistBuddy -c "Print :summary_results:pkg_creator_summary_result" "$ReportList" | grep "pkg_path" | awk '{print $3, $4}' | grep "$AppName")
+      PkgName=$(/usr/libexec/PlistBuddy -c "Print :summary_results:pkg_creator_summary_result" "$ReportList" | grep "pkg_path" | awk '{print $3, $4}' | grep "$AppName" | awk -F "/" '{print $6}')
+      AppAndVersion=$(/usr/libexec/PlistBuddy -c "Print :summary_results:pkg_creator_summary_result" "$ReportList" | grep "pkg_path" | awk '{print $3, $4}' | awk -F "/" '{print $6}' | awk -F "-" '{print $1,$2}' | sed 's/\.pkg//g' | grep "$AppName")
+      ReleaseVersion=$(/usr/libexec/PlistBuddy -c "Print :summary_results:pkg_creator_summary_result" "$ReportList" | grep "pkg_path" | awk '{print $3, $4}' | awk -F "/" '{print $6}' | awk -F "-" '{print $1,$2}' | sed 's/\.pkg//g' | grep "$AppName" | awk '{print $2}')
+      PkgUpload "$PkgName" "$PkgPath"
+      PatchDef "$AppName" "$AppPath"
+      PatchUpdate "$AppName" "$ReleaseVersion"
+      InstallUpdate "$PkgId" "$AppName"
+    done
+    #Slack Notification
+    AppAndVersion=$(/usr/libexec/PlistBuddy -c "Print :summary_results:pkg_creator_summary_result" "$ReportList" | grep "pkg_path" | awk '{print $3, $4}' | awk -F "/" '{print $6}' | awk -F "-" '{print $1,$2}' | sed 's/\.pkg//g')
+    SlackMessage="New Software Available: \n$AppAndVersion"
+    Payload="payload={\"channel\": \"$Channel\", \"username\": \"$Username\", \"text\": \"$SlackMessage\", \"icon_emoji\": \"$EMOJI\"}"
+    curl -s -X POST --data-urlencode "$Payload" "$SlackHook"
+  fi
+}
+
+AutoPkgCheck
+AutoPkgRun
+
+exit 0
---
+++ In-HouseUpdate.sh
@@ -0,0 +1,113 @@
+#!/bin/bash
+#
+# 11/10/19 Scott Gary
+#
+# In-House Automation
+################################################################################
+NexusUsername=""
+NexusPass=""
+NexusName="" # Nexus Name for repo (i.e unbox-gui)
+AppName=""
+BuildNum="" # ${VERSION}
+# Jamf Variables
+JSSUser=""
+JSSPass=""
+JamfServer=""
+#DevAdmin ID info
+DevId=""
+DevPw=""
+
+# Make needed DIR
+mkdir -p ROOT/Applications/Research\ Tools
+mkdir Scripts
+
+# Notarize function for reuse:
+NotarizeMe(){
+  NotarizePath="$1"
+  AppPath="$2"
+  # Send Notarization
+  Req=$(/usr/bin/xcrun altool --notarize-app \
+    --primary-bundle-id "io.mfj.$AppName" \
+    --username "$DevId" --password "$DevPw" \
+    --file "$NotarizePath" \
+    --output-format "xml")
+  ReqUUID=$(echo "$Req" | awk '/RequestUUID/ {getline;print}' | awk -F "<string>" '{print $2}' | awk -F "</string>" '{print $1}')
+  # Wait for reuest success
+  until [ "$ReqUUID" == "success" ]
+  do
+    ReqUUID=$(/usr/bin/xcrun altool --notarization-info "$ReqUUID" -u "$DevId" -p "$DevPw" | grep "Status:" | awk -F " " '{print $2}')
+  done
+  # Step 3: staple the ticket
+  sudo -u mfjadmin /usr/bin/xcrun stapler staple "$AppPath"
+}
+
+# Download new build:
+curl -s -u "$NexusUsername:$NexusPass" "https://nexus.mfj.io/repository/maven-releases/io/mfj/$NexusName/$BuildNum/$NexusName-$BuildNum-app.zip" > "$AppName.app.zip"
+# Unzip download and Notarize new release:
+unzip -qq "$AppName.app" -d "ROOT/Applications/Research Tools/"
+# Notarize App:
+NotarizeMe "$AppName.app.zip" "ROOT/Applications/Research Tools/$AppName.app"
+
+# Upload new patch to PatchServer
+curl -s -X POST \
+  "https://jamf-patch.mfj.io/api/v1/title/$AppName/version?=" \
+  -H 'Accept: */*' \
+  -H "Authorization: Bearer $JamfAPIToken" \
+  -H 'Content-Type: application/json' \
+  -H 'Host: jamf-patch.mfj.io' \
+  -d "$(python /Users/Shared/patchstarter.py ROOT/Applications/Research\ Tools/"$AppName".app --patch-only)"
+
+# Find preinstall script
+PreInstallScript=$(find /Users/Shared/PreInstallScripts/"$AppName"PreInstall.sh)
+cp -f "$PreInstallScript" Scripts/preinstall
+chmod a+x Scripts/preinstall
+
+# PKG and sign
+pkgbuild --quiet --root "ROOT" --sign "Measures for Justice (XUDASH3VHA)" --scripts Scripts "$AppName.$BuildNum.pkg"
+cp -f "$AppName.$BuildNum.pkg" "$HOME/Desktop/$AppName.$BuildNum.pkg"
+
+# Get pacage ready for upload:
+PkgName="$AppName.$BuildNum.pkg"
+PkgPath="$PWD/$AppName.$BuildNum.pkg"
+# Notarize PKG
+NotarizeMe "$PkgPath" "$PkgPath"
+
+#Upload to Jamf
+PkgUpload=$(curl -u $JSSUser:$JSSPass -X POST \
+  "$JamfServer"/dbfileupload \
+  -H 'DESTINATION: 0' \
+  -H 'OBJECT_ID: -1' \
+  -H 'FILE_TYPE: 0' \
+  -H 'FILE_NAME: '$PkgName \
+  -T $PkgPath )
+
+# Get New Package ID
+PkgId=$(echo "$PkgUpload" | awk -F "<id>" '{print $2}' | awk -F "</id" '{print $1}')
+
+# Check for new Patch Def in JamfServer
+PatchVersionId=$(cat /Users/Shared/AutoAutoPkg/InHousePatchTitles.txt | grep -i "$AppName" | awk -F " " '{print $2}' )
+JamfVersion=$(curl -sv -H "Accept: application/xml" -u "$JSSUser":"$JSSPass" "$JamfServer/JSSResource/patchsoftwaretitles/id/$PatchVersionId" -X GET)
+until [ "$BuildNum" == "$JamfVersion" ]
+do
+  JamfVersion=$(curl -sv -H "Accept: application/xml" -u "$JSSUser":"$JSSPass" "$JamfServer/JSSResource/patchsoftwaretitles/id/$PatchVersionId" -X GET)
+done
+
+# Update new definition:
+DefPkgChange="<patch_software_title><versions><version><software_version>$JamfVersion</software_version><package><id>$PkgId</id></package></version></versions></patch_software_title>"​
+curl -s -u $JSSUser:$JSSPass "$JamfServer/JSSResource/patchsoftwaretitles/id/$PatchVersionId" -X PUT \
+-H "Content-type: application/xml" \
+-d "<?xml version=\"1.0\" encoding=\"UTF-8\"?>$DefPkgChange"
+
+# Update install policy:
+InstallId=$(cat /Users/Shared/AutoAutoPkg/InHousePolicies.txt | grep -i "$AppName" | awk -F " " '{print $2}' )
+InstallPkgChange="<policy><package><package_configuration><packages><package><id>$PkgId</id></package></packages></package_configuration></policy>"​
+curl -s -u $JSSUser:$JSSPass "$JamfServer/JSSResource/policies/id/$InstallId" -X PUT \
+-H "Content-type: application/xml" \
+-d "<?xml version=\"1.0\" encoding=\"UTF-8\"?>$InstallPkgChange"
+
+# Update patch policy:
+PatchId=$(cat /Users/Shared/AutoAutoPkg/InHousePolicies.txt | grep -i "$AppName" | awk -F " " '{print $3}' )
+PatchChange="<patch_policy><general><target_version>$JamfVersion</target_version></general></patch_policy>"​
+curl -s -u $JSSUser:$JSSPass "$JamfServer/JSSResource/patchpolicies/id/$PatchId" -X PUT \
+-H "Content-type: application/xml" \
+-d "<?xml version=\"1.0\" encoding=\"UTF-8\"?>$PatchChange"
