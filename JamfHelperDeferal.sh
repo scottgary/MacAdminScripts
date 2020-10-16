cbd9c5416a04251e27c892b5033ca8250aef02d5
ScottGary <scott.gary@mfj.io>
2020-10-16 10:14:46 -0400

    Update JamfHelperDeferal.sh

--- JamfHelperDeferal.sh
+++ JamfHelperDeferal.sh
@@ -6,15 +6,14 @@
 #                                                                            #
 ##############################################################################
 # Global Vars:
-title="$4"
-heading="$5"
-text="$6"
-Defer1="$7"
-Defer2="$8"
-Defer3="$9"
-Defer4="${10}"
-Trigger="${11}"
-PolicyName=""
+heading="$4"
+text="$5"
+Defer1="$6"
+Defer2="$7"
+Defer3="$8"
+Defer4="$9"
+Trigger="${10}"
+PolicyName="${11}"
 PlistName="com.jamfsoftware.PolicyDeferal.$PolicyName.plist"
 ##############################################################################
 ##############################################################################
@@ -24,11 +23,13 @@ if [[ -z "$title" ]] || [[ -z "$heading" ]] || [[ -z "$text" ]] || [[ -z "$Defer
   exit 0
 fi
 
-# defer options
-buttonWithDelay=$("/Library/Application Support/JAMF/bin/jamfHelper.app/Contents/MacOS/jamfHelper" -windowType hud -title "Adjust your System Power Manager" -heading "$heading" -alignHeading justified -description "$text" -alignDescription left -icon /Applications/Self\ Service.app/Contents/Resources/AppIcon.icns -button1 'Select' -showDelayOptions "0, $Defer1, $Defer2, $Defer3, $Defer4" -timeout 120 -countdown -lockHUD)
+# Jamf Helper binary: Change -title to fit your org. This uses the -icon for Self Service for custom branding.
+buttonWithDelay=$("/Library/Application Support/JAMF/bin/jamfHelper.app/Contents/MacOS/jamfHelper" -windowType hud -title "Measures for Justice IT" -heading "$heading" -alignHeading justified -description "$text" -alignDescription left -icon /Applications/Self\ Service.app/Contents/Resources/AppIcon.icns -button1 'Select' -showDelayOptions "0, $Defer1, $Defer2, $Defer3, $Defer4" -timeout 120 -countdown -lockHUD)
 echo "$buttonWithDelay"
+# Take off Jamf Helper Button Return Code
 SelectedTime=${buttonWithDelay%?}
 echo "$SelectedTime"
+
 # Get Date for Daemon
 Epoch=$(date '+%s')
 Epoch2=$(( Epoch + SelectedTime ))
@@ -37,7 +38,7 @@ NewDay=$(date -jf %s "$Epoch2" "+%d")
 NewHour=$(date -jf %s "$Epoch2" "+%H")
 NewMinute=$(date -jf %s "$Epoch2" "+%M")
 
-
+# Write out Launch Daemon using StartCalendarInterval instead of StartInterval to make sure it still runs if computer rebooted
 LaunchDaemon='<?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
 <plist version="1.0">
@@ -67,6 +68,7 @@ LaunchDaemon='<?xml version="1.0" encoding="UTF-8"?>
 </dict>
 </plist>'
 
+# If user selected to delay:
 if [[ "$SelectedTime" == "$Defer1" ]] || [[ "$SelectedTime" == "$Defer2" ]] || [[ "$SelectedTime" == "$Defer3" ]] || [[ "$SelectedTime" == "$Defer4" ]]; then
   /usr/bin/tee "/Library/LaunchDaemons/$PlistName" << EOF
   $(echo "$LaunchDaemon")
@@ -76,9 +78,11 @@ EOF
   /bin/chmod 644 "/Library/LaunchDaemons/$PlistName"
   /bin/launchctl bootstrap system "/Library/LaunchDaemons/$PlistName"
 elif [[ "$SelectedTime" = 0 ]]; then
+  # User selected to run immediately
   echo "running now"
   /usr/local/bin/jamf policy -event "$Trigger"
 else
+  # If countdown runs out or other errors exist
   echo "Something went wrong!"
 fi
 
